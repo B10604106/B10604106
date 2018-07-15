edf2be04b9c0cce444853c9234e7cf8ea0eecf9e
B10604106 <40812743+B10604106@users.noreply.github.com>
2018-07-15 16:35:54 +0800

    add

--- week2/ToothGrowth/ToothGrowth.R
+++ week2/ToothGrowth/ToothGrowth.R
@@ -3,3 +3,7 @@ library(dplyr)
 library(ggplot2)
 ggplot(data = ToothGrowth, aes( x = len, y = supp, color = dose)) + 
   geom_point() 
+
+#The Effect of Vitamin C on Tooth Growth in Guinea Pigs
+##資料中的levels分兩種一種是"OJ"，一種是"VC"
+##顏色表示劑量
