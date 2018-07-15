1f0c6b8a95f5c72b353cb0eba8c5b852503c5c0a
B10604106 <40812743+B10604106@users.noreply.github.com>
2018-07-15 17:20:19 +0800

    add

--- week2/InsectSprays/InsectSprays.R
+++ week2/InsectSprays/InsectSprays.R
@@ -1,6 +1,8 @@
 library(datasets)
 library(dplyr)
 library(ggplot2)
-ggplot(data = InsectSprays, aes( x = count)) + 
+ggplot(data = InsectSprays, aes( x = factor)) + 
   geom_bar(fill = "lightblue", colour = "black") 
 
+
+str(InsectSprays)
